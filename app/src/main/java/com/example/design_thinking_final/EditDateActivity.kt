8fdd7333ab9249dda3961f5c96cbf65b1eb6dbac
tlsgnvkr <shinhup4743@gmail.com>
2023-12-06 12:58:10 +0900

    Initial commit

---
+++ app/src/main/java/com/example/design_thinking_final/EditDateActivity.kt
@@ -0,0 +1,11 @@
+package com.example.design_thinking_final
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditDateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_date)
+    }
+}
